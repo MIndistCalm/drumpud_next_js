e21273ae2e51c01d98cfc330f8ebac996a6c66ba
MindistCalm <51086034+MIndistCalm@users.noreply.github.com>
2024-09-14 14:30:29 +0700

    added Home page and drumpad page (#1) (#2)
    Co-authored-by: MindistCalm <suxinin4@yandex.ru>

---
+++ src/app/drumpad/page.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import { FC } from 'react'
+
+export const Drumpad: FC = () => {
+  return (
+    <div className='min-h-screen p-6'>
+      <Link href='/' className='flex gap-4 items-center'>
+        <span className='text-3xl'>←</span>
+        <span className='text-4xl'>Назад на главную</span>
+      </Link>
+      <div className='flex items-center justify-centertext-3xl full'>
+        <span>Тут будет Дрампад</span>
+      </div>
+    </div>
+  )
+}
+
+export default Drumpad
